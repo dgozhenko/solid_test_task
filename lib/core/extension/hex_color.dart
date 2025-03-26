6620ae5bb3b229764556e6efc887cdec9b84395b
dgozhenko <daniil.gozhenko@gmail.com>
2025-03-26 21:42:27 +0200

    Fix warnings

--- lib/core/extension/hex_color.dart
+++ lib/core/extension/hex_color.dart
@@ -1,16 +1,40 @@
 import 'dart:ui';
 
+/// Extension on Color to convert Color to hex string
+/// and hex string to Color
 extension HexColor on Color {
+  /// method to convert hex string into color
+  /// can accept hex string with # and without
   static Color fromHex(String hexString) {
     final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    const hexStringWithoutHexLength = 6;
+    const hexStringWithHexLength = 6;
+    const radix = 16;
+
+    if (hexString.length == hexStringWithoutHexLength ||
+        hexString.length == hexStringWithHexLength) {
+      buffer.write('ff');
+    }
     buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+
+    return Color(int.parse(buffer.toString(), radix: radix));
   }
 
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${(255 * a).toInt().toRadixString(16).padLeft(2, '0')}'
-      '${(255 * r).toInt().toRadixString(16).padLeft(2, '0')}'
-      '${(255 * g).toInt().toRadixString(16).padLeft(2, '0')}'
-      '${(255 * b).toInt().toRadixString(16).padLeft(2, '0')}';
+  /// method to convert color into hex string
+  /// can generate string with leading hex or without it
+  String toHex({bool leadingHashSign = true}) {
+    const maxIntValue = 255;
+    const radix = 16;
+    const padAmount = 2;
+
+    return '${leadingHashSign ? '#' : ''}'
+        '${(maxIntValue * a).toInt().toRadixString(radix)
+              .padLeft(padAmount, '0')}'
+        '${(maxIntValue * r).toInt().toRadixString(radix)
+             .padLeft(padAmount, '0')}'
+        '${(maxIntValue * g).toInt().toRadixString(radix)
+             .padLeft(padAmount, '0')}'
+        '${(maxIntValue * b).toInt().toRadixString(radix)
+              .padLeft(padAmount, '0')}';
+  }
 }
