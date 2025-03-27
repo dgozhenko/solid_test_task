12a599cdfb254594870a33f25976036f9867c24f
dgozhenko <daniil.gozhenko@gmail.com>
2025-03-27 02:10:20 +0200

    Add ignore for unused parameters

--- lib/presentation/color_gallery/cubit/color_gallery_state.dart
+++ lib/presentation/color_gallery/cubit/color_gallery_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_unused_parameters
 part of 'color_gallery_cubit.dart';
 
 /// State for Color Randomizer Cubit
--- lib/presentation/color_randomizer/cubit/color_randomizer_state.dart
+++ lib/presentation/color_randomizer/cubit/color_randomizer_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_unused_parameters
 part of 'color_randomizer_cubit.dart';
 
 /// State for Color Randomizer Cubit
