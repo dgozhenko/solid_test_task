9535843c808fca0728d080c312a19c69da9efe34
dgozhenko <daniil.gozhenko@gmail.com>
2025-03-27 14:31:52 +0200

    Add color gallery cubit test

---
+++ test/color_gallery_cubit_test.dart
@@ -0,0 +1,98 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:solid_test_task/domain/model/color_model.dart';
+import 'package:solid_test_task/domain/repository/color_repository.dart';
+import 'package:solid_test_task/presentation/color_gallery/cubit/color_gallery_cubit.dart';
+
+import 'color_gallery_cubit_test.mocks.dart';
+
+@GenerateMocks([ColorRepository])
+void main() {
+  late ColorGalleryCubit colorGalleryCubit;
+  late MockColorRepository colorRepository;
+
+  setUp(() {
+    colorRepository = MockColorRepository();
+    colorGalleryCubit = ColorGalleryCubit(colorRepository: colorRepository);
+  });
+
+  tearDown(() {
+    colorGalleryCubit.close();
+  });
+
+  test('Cubit in initial state', () {
+    expect(
+      colorGalleryCubit.state,
+      const ColorGalleryState(colors: [], loading: false),
+    );
+  });
+
+  blocTest<ColorGalleryCubit, ColorGalleryState>(
+    'emits state with loading then emit colors and loading == false',
+    build: () {
+      when(
+        colorRepository.getColors(),
+      ).thenAnswer((_) async => [const ColorModel(hexString: '#ffffffff')]);
+
+      return colorGalleryCubit;
+    },
+    act: (cubit) {
+      cubit.getColors();
+    },
+    expect:
+        () => [
+          const ColorGalleryState(loading: true, colors: []),
+          const ColorGalleryState(
+            loading: false,
+            colors: [ColorModel(hexString: '#ffffffff')],
+          ),
+        ],
+  );
+
+  blocTest<ColorGalleryCubit, ColorGalleryState>(
+    'emits state with loading then emit empty colors and loading == false',
+    build: () {
+      when(
+        colorRepository.getColors(),
+      ).thenAnswer((_) async => []);
+
+      return colorGalleryCubit;
+    },
+    act: (cubit) {
+      cubit.getColors();
+    },
+    expect:
+        () => [
+      const ColorGalleryState(loading: true, colors: []),
+      const ColorGalleryState(
+        loading: false,
+        colors: [],
+      ),
+    ],
+  );
+
+  blocTest<ColorGalleryCubit, ColorGalleryState>(
+    'emits state with loading then emit error and loading == false',
+    build: () {
+      when(
+        colorRepository.getColors(),
+      ).thenThrow(Exception('Cannot access Database'));
+
+      return colorGalleryCubit;
+    },
+    act: (cubit) {
+      cubit.getColors();
+    },
+    expect:
+        () => [
+      const ColorGalleryState(loading: true, colors: []),
+      const ColorGalleryState(
+        loading: false,
+        colors: [],
+        error: 'Exception: Cannot access Database'
+      ),
+    ],
+  );
+}
